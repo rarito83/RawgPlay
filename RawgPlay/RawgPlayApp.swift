95c86679edd835355fd4c38393c4942a9b073b96
rarito83 <rarito.jogja@gmail.com>
2023-02-28 14:43:43 +0700

    Initial Commit

---
+++ RawgPlay/RawgPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  RawgPlayApp.swift
+//  RawgPlay
+//
+//  Created by Rarito on 28/02/23.
+//
+
+import SwiftUI
+
+@main
+struct RawgPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
